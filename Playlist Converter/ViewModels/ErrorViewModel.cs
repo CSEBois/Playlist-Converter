f947feb8d1d2a4815dfba2b0ae216d4358875647
Josh Seltzer <josh.a.seltzer@gmail.com>
2020-03-12 07:43:07 -0400

    Checking if all new files can be added to the repo
    Created new file structure to account for user authentication

---
+++ Playlist Converter/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Playlist_Converter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Playlist Converter/wwwroot/js/login.js
@@ -0,0 +1,85 @@
+﻿(function () {
+    //
+    var userCountry = "US";
+    var localApi = new localProxyApi(serverBasePath);
+    var spotifyWebApi = new SpotifyWebApi();
+
+
+
+
+
+
+    //Following 3 funtcions taken from artist explorer main.js file
+
+    function login() {
+        return new Promise(function (resolve, reject) {
+            OAuthManager.obtainToken({
+                scopes: [
+                    'playlist-read-private',
+                    'playlist-modify-public',
+                    'playlist-modify-private'
+                ]
+            }).then(function (token) {
+                resolve(onTokenReceived(token));
+            }).catch(function (error) {
+                console.error(error);
+            });
+        });
+    }
+    function getDisplayName(str) {
+        var maxDisplayLength = 11;
+        if (str.length < maxDisplayLength) {
+            return str;
+        }
+
+        var spaceIndex = str.indexOf(' ');
+        if (spaceIndex != -1 && spaceIndex < maxDisplayLength) {
+            return str.substr(0, spaceIndex);
+        }
+        return str.substr(0, maxDisplayLength);
+    }
+
+    function onTokenReceived(accessToken) {
+        return new Promise(function (resolve, reject) {
+            artistInfoModel.isLoggedIn(true);
+            spotifyWebApi.setAccessToken(accessToken);
+            localStorage.setItem('ae_token', accessToken);
+            localStorage.setItem('ae_expires', (new Date()).getTime() + 3600 * 1000); // 1 hour
+            spotifyWebApi.getMe().then(function (data) {
+                artistInfoModel.userId(data.id);
+                artistInfoModel.displayName(getDisplayName(data.display_name));
+                artistInfoModel.userImage(data.images[0].url);
+                localStorage.setItem('ae_userid', data.id);
+                localStorage.setItem('ae_display_name', data.display_name);
+                localStorage.setItem('ae_user_image', data.images[0].url);
+                currentApi = spotifyWebApi;
+                resolve(checkArtistExplorerPlaylistExists(artistInfoModel.userId(), 0, 50));
+            });
+
+        });
+    }
+
+    window.AE = {
+        getSuitableImage: getSuitableImage,
+        getRelated: getRelated,
+        getArtistsForGenre: getArtistsForGenre,
+        getInfoCancel: getInfoCancel,
+        getInfo: getInfo,
+        changeNumberOfArtists: changeNumberOfArtists,
+        setRepeatArtists: setRepeatArtists,
+        toTitleCase: toTitleCase,
+        artistInfoModel: artistInfoModel,
+        login: login,
+        createPlaylistModal: createPlaylistModal,
+        createPlaylist: createPlaylist,
+        logout: logout
+    };
+
+
+
+
+
+
+
+
+})
