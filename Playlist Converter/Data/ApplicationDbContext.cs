3e38d858574758bc79610f054d958c7f6d913c03
Josh Seltzer <josh.a.seltzer@gmail.com>
2020-04-17 17:52:01 -0500

    Remove ignored files

--- Playlist Converter/Data/ApplicationDbContext.cs
+++ Playlist Converter/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Playlist_Converter.Models;
 
 namespace Playlist_Converter.Data
 {
@@ -12,5 +13,6 @@ namespace Playlist_Converter.Data
             : base(options)
         {
         }
+        public DbSet<Playlist_Converter.Models.AccountModel> AccountModel { get; set; }
     }
 }
---
+++ Playlist Converter/Models/AccountModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Playlist_Converter.Models
+{
+    public class AccountModel
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+
+    }
+    //Need to finish going through the model and find all the properties needed for the DB
+    //Need to create controller for Accounts to access the correct pages from the login partial
+    //Need to create folder for login 
+}
